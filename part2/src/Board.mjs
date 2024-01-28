8ada24d6e1eff3aabe582a7c836df4112d78c511
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-01-28 18:48:44 +0100

    Implement board toString

--- part2/src/Board.mjs
+++ part2/src/Board.mjs
@@ -8,6 +8,13 @@ export class Board {
   }
 
   toString() {
-    return "TODO";
+    let boardString = "";
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        boardString += ".";
+      }
+      boardString += "\n";
+    }
+    return boardString;
   }
 }
