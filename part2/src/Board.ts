336dec3bf76f9a384bdaa1ed24f6507caadf2ab5
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-01-28 19:05:27 +0100

    Implement naive drop

--- part2/src/Board.ts
+++ part2/src/Board.ts
@@ -1,6 +1,9 @@
 export class Board {
   width;
   height;
+  shape = "";
+  shapeRow = 0;
+  shapeColumn = 0;
 
   constructor(width: number, height: number) {
     this.width = width;
@@ -8,13 +11,19 @@ export class Board {
   }
 
   drop(shape: string) {
-    return;
+    this.shape = shape;
+    this.shapeRow = 0;
+    this.shapeColumn = 1;
   }
 
   toString(): string {
     let boardString = "";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
+        if (this.shape.length > 0 && this.shapeRow === i && this.shapeColumn === j) {
+          boardString += this.shape;
+          continue;
+        }
         boardString += ".";
       }
       boardString += "\n";
