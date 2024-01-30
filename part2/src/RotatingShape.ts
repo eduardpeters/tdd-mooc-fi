872026629467c5d117f8b438b5cd44e470d2d33e
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-01-30 20:30:47 +0100

    Implement rotateRight

--- part2/src/RotatingShape.ts
+++ part2/src/RotatingShape.ts
@@ -2,11 +2,18 @@ export class RotatingShape {
   shape: string;
 
   constructor(shape: string) {
-    console.log(shape);
     this.shape = shape;
   }
 
   toString() {
     return this.shape + "\n";
+  }
+
+  rotateRight(): RotatingShape {
+    const rows = this.shape.split("\n");
+    let rotated = `${rows[2][0]}${rows[1][0]}${rows[0][0]}\n`;
+    rotated += `${rows[2][1]}${rows[1][1]}${rows[0][1]}\n`;
+    rotated += `${rows[2][2]}${rows[1][2]}${rows[0][2]}`;
+    return new RotatingShape(rotated);
   }
 }
