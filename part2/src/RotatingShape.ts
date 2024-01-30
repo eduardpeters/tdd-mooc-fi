f0a8d75dc4fbecf7f73e069e59e56bc8f7ec1a2e
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-01-30 20:45:50 +0100

    Refactor rotateRight to be generic

--- part2/src/RotatingShape.ts
+++ part2/src/RotatingShape.ts
@@ -11,9 +11,13 @@ export class RotatingShape {
 
   rotateRight(): RotatingShape {
     const rows = this.shape.split("\n");
-    let rotated = `${rows[2][0]}${rows[1][0]}${rows[0][0]}\n`;
-    rotated += `${rows[2][1]}${rows[1][1]}${rows[0][1]}\n`;
-    rotated += `${rows[2][2]}${rows[1][2]}${rows[0][2]}`;
+    let rotated = "";
+    for (let i = 0; i < rows[0].length; i++) {
+      for (let j = rows.length - 1; j >= 0; j--) {
+        rotated += rows[j][i];
+      }
+      if (i !== rows[0].length - 1) rotated += "\n";
+    }
     return new RotatingShape(rotated);
   }
 
