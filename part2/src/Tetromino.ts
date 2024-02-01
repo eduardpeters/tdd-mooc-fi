b546ae52527faf483763d59d9a2d5643796fa5d2
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-01 21:19:11 +0100

    Refactor to remove shape property

--- part2/src/Tetromino.ts
+++ part2/src/Tetromino.ts
@@ -1,30 +1,29 @@
 import { RotatingShape } from "./RotatingShape";
 
 export class Tetromino {
-  shape: RotatingShape;
   orientations: RotatingShape[];
+  currentOrientation: number;
 
   static T_SHAPE = new Tetromino(`.T.\nTTT\n...`, 4);
   static I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....`, 2);
 
   constructor(shape: string, orientationCount: number) {
-    this.shape = new RotatingShape(shape);
-    this.orientations = [];
-    for (let c = 0; c < orientationCount; c++) {
-      this.orientations.push(this.shape);
-      this.shape = this.shape.rotateRight();
+    this.orientations = [new RotatingShape(shape)];
+    this.currentOrientation = 0;
+    for (let i = 1; i < orientationCount; i++) {
+      this.orientations.push(this.orientations[i]);
     }
   }
 
   rotateRight() {
-    return this.shape.rotateRight();
+    return this.orientations[this.currentOrientation].rotateRight();
   }
 
   rotateLeft() {
-    return this.shape.rotateLeft();
+    return this.orientations[this.currentOrientation].rotateLeft();
   }
 
   toString() {
-    return this.shape.toString();
+    return this.orientations[this.currentOrientation].toString();
   }
 }
