6fea5cc1b570b6a003483a6bba2e59a50820a03a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-05 18:54:35 +0100

    Add MovingTetrominoes test file

---
+++ part2/test/MovingTetrominoes.test.mjs
@@ -0,0 +1,25 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.ts";
+import { Tetromino } from "../src/Tetromino.ts";
+
+describe("Moving tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("tetromino can be moved left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+});
