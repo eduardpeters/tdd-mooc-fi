716b3c60663a397aa57fee323bb6ad183d535c2f
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-08 20:31:06 +0100

    Test for falling tetromino left rotation

---
+++ part2/test/RotatingFallingTetrominoes.test.mjs
@@ -0,0 +1,43 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.ts";
+import { Tetromino } from "../src/Tetromino.ts";
+
+function moveToLeft(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveLeft();
+  }
+}
+
+function moveToRight(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveRight();
+  }
+}
+
+function moveToBottom(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveDown();
+  }
+}
+
+describe("Rotaring falling tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("a falling tetromino can be rotated left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TT......
+       ...T......
+       ..........
+       ..........
+       ..........`
+    );
+  });
+});
