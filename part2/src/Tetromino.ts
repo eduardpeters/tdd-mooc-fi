eb2d0edd4d1218fe581bcf393052b77623017085
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-15 20:49:44 +0100

    Update I tetromino

--- part2/src/Tetromino.ts
+++ part2/src/Tetromino.ts
@@ -1,17 +1,18 @@
 import { RotatingShape } from "./RotatingShape";
 
 export class Tetromino {
-  shape: string;
   size: number;
   orientations: RotatingShape[];
   currentOrientation: number;
 
   static T_SHAPE = new Tetromino(`.T.\nTTT\n...`, 4);
-  static I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....`, 2);
+  static I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....`, 2, [
+    new RotatingShape("....\nIIII\n....\n...."),
+    new RotatingShape("..I.\n..I.\n..I.\n..I."),
+  ]);
   static O_SHAPE = new Tetromino(`.OO\n.OO\n...`, 1);
 
   constructor(shape: string, orientationCount: number, orientations: RotatingShape[] = [], currentOrientation = 0) {
-    this.shape = shape;
     this.orientations = orientations;
     this.currentOrientation = currentOrientation;
     if (this.orientations.length === 0) {
