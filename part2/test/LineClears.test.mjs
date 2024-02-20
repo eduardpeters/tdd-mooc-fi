21a9c2feef98159ad1398952ae300ae1dc51efaa
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-20 20:50:08 +0100

    Add LineClears test file

---
+++ part2/test/LineClears.test.mjs
@@ -0,0 +1,43 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.ts";
+import { Tetromino } from "../src/Tetromino.ts";
+
+function moveToLeft(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveLeft();
+  }
+}
+
+function moveToRight(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveRight();
+  }
+}
+
+function moveToBottom(board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveDown();
+  }
+}
+
+describe("Line clears", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test.skip("A filled line at bottom is cleared", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
+
+    expect(board.toString()).to.equalShape(
+      `..TTT.....
+       ...T......
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+});
