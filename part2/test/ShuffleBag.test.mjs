20e36f6b42cbd91c111db212d0d0503d4d239e49
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-25 10:28:36 +0100

    Add tetromino tests to shuffle bag

--- part2/test/ShuffleBag.test.mjs
+++ part2/test/ShuffleBag.test.mjs
@@ -1,9 +1,9 @@
 import { beforeEach, describe, test, expect } from "vitest";
+import { Tetromino } from "../src/Tetromino";
 import { ShuffleBag } from "../src/ShuffleBag";
 
-const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-describe("Shuffle bag", () => {
+describe("Shuffle bag with numbers", () => {
+  const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let bag;
   beforeEach(() => {
     bag = new ShuffleBag([...values]);
@@ -20,5 +20,33 @@ describe("Shuffle bag", () => {
     }
     outputs.sort((a, b) => a - b);
     expect(outputs).toEqual(values);
+  });
+});
+
+describe("Shuffle bag with numbers", () => {
+  const values = [
+    Tetromino.I_SHAPE,
+    Tetromino.J_SHAPE,
+    Tetromino.L_SHAPE,
+    Tetromino.O_SHAPE,
+    Tetromino.S_SHAPE,
+    Tetromino.T_SHAPE,
+    Tetromino.Z_SHAPE,
+  ];
+  let bag;
+  beforeEach(() => {
+    bag = new ShuffleBag([...values]);
+  });
+
+  test("A shufflebag is filled and returns a valid output", () => {
+    expect(values.includes(bag.next())).toBe(true);
+  });
+
+  test("A full run exhausts all possible Tetrominoes", () => {
+    let outputs = [];
+    for (let i = 0; i < values.length; i++) {
+      outputs.push(bag.next());
+    }
+    expect(outputs.sort()).toEqual(values.sort());
   });
 });
