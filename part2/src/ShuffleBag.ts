e158188b343fce4bdd88d34b7d7170704a1316ba
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-25 10:20:15 +0100

    Implement next method

--- part2/src/ShuffleBag.ts
+++ part2/src/ShuffleBag.ts
@@ -9,7 +9,17 @@ export class ShuffleBag<T> {
     }
   }
 
-  next() {
-    return 1;
+  next(): T {
+    if (this.currentPosition < 1) {
+      this.currentPosition = this.data.length - 1;
+      return this.data[0];
+    } else {
+      const nextPosition = Math.floor(Math.random() * this.currentPosition);
+      const nextItem = this.data[nextPosition];
+      this.data[nextPosition] = this.data[this.currentPosition];
+      this.data[this.currentPosition] = nextItem;
+      this.currentPosition--;
+      return nextItem;
+    }
   }
 }
--- part2/test/ShuffleBag.test.mjs
+++ part2/test/ShuffleBag.test.mjs
@@ -1,15 +1,24 @@
 import { beforeEach, describe, test, expect } from "vitest";
 import { ShuffleBag } from "../src/ShuffleBag";
 
-const values = [0, 1, 2, 3, 4, 5, 6, 8, 9];
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 describe("Shuffle bag", () => {
   let bag;
   beforeEach(() => {
-    bag = new ShuffleBag();
+    bag = new ShuffleBag([...values]);
   });
 
   test("A shufflebag is filled and returns a valid output", () => {
     expect(values.includes(bag.next())).toBe(true);
+  });
+
+  test("A full run exhausts all possible numbers", () => {
+    let outputs = [];
+    for (let i = 0; i < values.length; i++) {
+      outputs.push(bag.next());
+    }
+    outputs.sort((a, b) => a - b);
+    expect(outputs).toEqual(values);
   });
 });
