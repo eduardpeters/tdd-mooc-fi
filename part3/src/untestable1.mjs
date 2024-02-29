e44bd57cc55d7112dab6ab5797a9eab7abdbdc37
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-29 21:07:49 +0100

    Passes a day after Christmas

--- part3/src/untestable1.mjs
+++ part3/src/untestable1.mjs
@@ -4,7 +4,7 @@ export function daysUntilChristmas(currentDate) {
   const today = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
   const christmasDay = new Date(currentDate.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
-    christmasDay.setFullYear(new Date().getFullYear() + 1);
+    christmasDay.setFullYear(currentDate.getFullYear() + 1);
   }
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
--- part3/test/untestable1.test.mjs
+++ part3/test/untestable1.test.mjs
@@ -7,4 +7,16 @@ describe("Untestable 1: days until Christmas", () => {
     // TODO: write proper tests
     expect(daysUntilChristmas(new Date("2024-12-25"))).to.equal(0);
   });
+
+  test("Returns correct number of days on Christmas Eve", () => {
+    expect(daysUntilChristmas(new Date("2023-12-24"))).to.equal(1);
+  });
+
+  test("Returns correct number of days on New Year", () => {
+    expect(daysUntilChristmas(new Date("2024-01-01"))).to.equal(359);
+  });
+
+  test("Returns correct number of days the day after Christmas", () => {
+    expect(daysUntilChristmas(new Date("2023-12-26"))).to.equal(365);
+  });
 });
