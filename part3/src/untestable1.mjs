8f5110229dd938ac769c7e665cebeff25df6fd27
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-02-29 21:19:13 +0100

    Refactor to remove today variable

--- part3/src/untestable1.mjs
+++ part3/src/untestable1.mjs
@@ -1,12 +1,12 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
 export function daysUntilChristmas(currentDate) {
-  const today = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  currentDate.setMinutes(currentDate.getMinutes() + currentDate.getTimezoneOffset());
   const christmasDay = new Date(currentDate.getFullYear(), 12 - 1, 25);
-  if (today.getTime() > christmasDay.getTime()) {
+  if (currentDate.getTime() > christmasDay.getTime()) {
     christmasDay.setFullYear(currentDate.getFullYear() + 1);
   }
-  const diffMillis = christmasDay.getTime() - today.getTime();
+  const diffMillis = christmasDay.getTime() - currentDate.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
 
--- part3/test/untestable1.test.mjs
+++ part3/test/untestable1.test.mjs
@@ -4,7 +4,6 @@ import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
   test("Returns correct number of days from Christmas itself", () => {
-    // TODO: write proper tests
     expect(daysUntilChristmas(new Date("2024-12-25"))).to.equal(0);
   });
 
