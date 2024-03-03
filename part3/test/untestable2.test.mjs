253752a01c5515a24e668f741901742da865eaf9
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-03-03 21:09:40 +0100

    Test random dice rolls

--- part3/test/untestable2.test.mjs
+++ part3/test/untestable2.test.mjs
@@ -1,8 +1,17 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/untestable2.mjs";
+import { diceHandValue, diceRoll } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
+  test("Rolls lie between 1 and 6", () => {
+    const rolls = new Set();
+    for (let i = 0; i < 100; i++) {
+      rolls.add(diceRoll());
+    }
+    expect(rolls.size).to.equal(6);
+    expect(rolls).to.have.all.keys([1, 2, 3, 4, 5, 6]);
+  });
+
   test("One Pair", () => {
     expect(diceHandValue(3, 3)).to.equal(103);
     expect(diceHandValue(5, 5)).to.equal(105);
