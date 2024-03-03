f908bb2b497dcea5561e045fb0080aabe33f75d9
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-03-03 21:05:36 +0100

    Provide further tests for hand values

--- part3/test/untestable2.test.mjs
+++ part3/test/untestable2.test.mjs
@@ -5,9 +5,13 @@ import { diceHandValue } from "../src/untestable2.mjs";
 describe("Untestable 2: a dice game", () => {
   test("One Pair", () => {
     expect(diceHandValue(3, 3)).to.equal(103);
+    expect(diceHandValue(5, 5)).to.equal(105);
+    expect(diceHandValue(1, 1)).to.equal(101);
   });
 
   test("High dice", () => {
     expect(diceHandValue(3, 6)).to.equal(6);
+    expect(diceHandValue(1, 4)).to.equal(4);
+    expect(diceHandValue(3, 1)).to.equal(3);
   });
 });
