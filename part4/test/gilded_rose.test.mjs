17979b542ea09acca5d2a2b2334cf325e6330cc3
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-03-12 18:13:12 +0100

    Test Aged Brie

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -10,4 +10,23 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).to.equal(-1);
     expect(items[0].quality).to.equal(0);
   });
+
+  test("Aged Brie increases in quality within limits", () => {
+    const gildedRose = new Shop([provideAgedBrie(5, 10), provideAgedBrie(1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11);
+    expect(items[1].quality).to.equal(50);
+  });
 });
+
+function provideAgedBrie(sellIn = 10, quality = 25) {
+  return new Item("Aged Brie", sellIn, quality);
+}
+
+function provideBackstagePass(sellIn = 10, quality = 25) {
+  return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+}
+
+function provideSulfuras(sellIn = 10, quality = 25) {
+  return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
+}
