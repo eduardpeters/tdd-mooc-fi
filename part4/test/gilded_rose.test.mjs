91224f5e82d11a853b0fec6666ac56699fc34425
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-03-12 18:07:42 +0100

    Test update quality

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -3,9 +3,11 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
-  test("foo", () => {
+  test("Shop update quality method", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
   });
 });
