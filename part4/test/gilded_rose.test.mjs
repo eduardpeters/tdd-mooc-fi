76c482600c921a9ba6f7e264b0997083d3c4f7c2
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-03-16 09:08:59 +0100

    Refactor tests to use derived classes

--- part4/test/gilded_rose.test.mjs
+++ part4/test/gilded_rose.test.mjs
@@ -14,7 +14,7 @@ describe("Gilded Rose", () => {
     expect(items[1].quality).to.equal(8);
   });
 
-  test.skip("Aged Brie increases in quality within limits", () => {
+  test("Aged Brie increases in quality within limits", () => {
     const gildedRose = new Shop([provideAgedBrie(5, 10), provideAgedBrie(1, 50), provideAgedBrie(0, 25)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(11);
@@ -22,14 +22,14 @@ describe("Gilded Rose", () => {
     expect(items[2].quality).to.equal(27);
   });
 
-  test.skip("Sulfuras does not change sellIn or quality", () => {
+  test("Sulfuras does not change sellIn or quality", () => {
     const gildedRose = new Shop([provideSulfuras(100)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).to.equal(100);
     expect(items[0].quality).to.equal(80);
   });
 
-  test.skip("Backstage passes have different quality increases", () => {
+  test("Backstage passes have different quality increases", () => {
     const gildedRose = new Shop([
       provideBackstagePass(11, 10),
       provideBackstagePass(10, 10),
@@ -43,7 +43,7 @@ describe("Gilded Rose", () => {
     expect(items[3].quality).to.equal(0);
   });
 
-  test.skip("Backstage passes obey the quality limits", () => {
+  test("Backstage passes obey the quality limits", () => {
     const gildedRose = new Shop([
       provideBackstagePass(11, 50),
       provideBackstagePass(10, 50),
@@ -109,7 +109,7 @@ describe("Aged Brie", () => {
 
 describe("Sulfuras, Hand of Ragnaros", () => {
   test("Sulfuras has no changes with update", () => {
-    const sulfuras = new Sulfuras(5, 80);
+    const sulfuras = new Sulfuras(5);
     sulfuras.updateQuality();
     expect(sulfuras.sellIn).to.equal(5);
     expect(sulfuras.quality).to.equal(80);
@@ -159,13 +159,13 @@ function provideItem(name, sellIn = 10, quality = 25) {
 }
 
 function provideAgedBrie(sellIn = 10, quality = 25) {
-  return new Item("Aged Brie", sellIn, quality);
+  return new AgedBrie(sellIn, quality);
 }
 
 function provideBackstagePass(sellIn = 10, quality = 25) {
-  return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+  return new BackstagePass(sellIn, quality);
 }
 
 function provideSulfuras(sellIn = 10) {
-  return new Item("Sulfuras, Hand of Ragnaros", sellIn, 80);
+  return new Sulfuras(sellIn);
 }
